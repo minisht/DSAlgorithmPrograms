93a02c1f7c64eae79ca1118c0911bc0cd975c6f5
Minish Tera <minishtera@Minishs-MacBook-Pro.local>
2017-12-04 23:43:04 -0600

    Added insertionsort and inheritancechallenge

---
+++ src/com/inheritancechallenge/Main.java
@@ -0,0 +1,18 @@
+package com.inheritancechallenge;
+
+/**
+ * Created by minishtera on 12/4/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
+
+    }
+}
+
